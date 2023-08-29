2d332476e83f78ee173f3a8526b342eaf3cc3ecc
Lucas Maciel <farias.maciel@outlook.com>
2023-08-28 23:13:12 -0400

    criando model de Transaction

---
+++ src/models/Transaction.js
@@ -0,0 +1,29 @@
+const {DataTypes} = require('sequelize')
+
+const User = require('./User')
+
+const db = require('../db/conn')
+
+const Transaction = db.define('Transaction', {
+    value: {
+        type: DataTypes.DECIMAL(10, 2),
+        required: true,
+        allowNull: false,
+    },
+    type: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false
+    },
+    // receiverId: {
+    //     type: 
+    // }
+})
+
+User.hasMany(Transaction, { foreignKey: 'senderId', as: 'sentTransactions' });
+User.hasMany(Transaction, { foreignKey: 'receiverId', as: 'receivedTransactions' });
+
+Transaction.belongsTo(User, { foreignKey: 'senderId', as: 'sender' });
+Transaction.belongsTo(User, { foreignKey: 'receiverId', as: 'receiver' });
+
+module.exports = Transaction
