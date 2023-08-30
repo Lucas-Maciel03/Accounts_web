2591e17abeda1de8b0dcffe9f50a874b87ef250c
Lucas Maciel <farias.maciel@outlook.com>
2023-08-29 22:03:43 -0400

    criando controllers e routes de depositos

--- src/controllers/AccountsController.js
+++ src/controllers/AccountsController.js
@@ -5,4 +5,40 @@ module.exports = class AccountsController{
     static showTransactions(req, res){
         res.render('transactions/home')
     }
+
+    static deposit(req, res){
+        res.render('transactions/deposit')
+    }
+
+    static async depositPost(req, res){
+        const amount = parseFloat(req.body.amount)
+        const type = req.body.type
+        const id = req.session.userid   
+        
+        if(isNaN(amount)){
+            req.flash('message', 'O valor informado não é um número, tente novamente!')
+            res.render('transactions/deposit')
+        }
+        
+        try {
+            //Inserindo deposito na tabela User
+            const userData = await User.findOne({where: {id}, raw: true})
+            const user = {
+                balance: amount + parseFloat(userData.balance)
+            } 
+
+            await User.update(user, {where: {id}})
+
+            //Criando registro de deposito na tabela transaction
+            await Transaction.create({amount, type, receiverId: id})
+
+            req.flash('message', 'Deposito feito com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/accounts/deposit')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
--- src/routes/accountsRoutes.js
+++ src/routes/accountsRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const AccountsController = require('../controllers/AccountsController')
 const checkAuth = require('../helpers/checkAuth').checkAuth
 
+router.get('/deposit', checkAuth, AccountsController.deposit)
+router.post('/deposit', checkAuth, AccountsController.depositPost)
 router.get('/', AccountsController.showTransactions)
 
 module.exports = router
