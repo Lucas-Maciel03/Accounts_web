642552b0565a65f49cb4dc2028bd4d9a074fd4d6
Lucas Maciel <farias.maciel@outlook.com>
2023-08-30 22:58:49 -0400

    criando validações no controller de saque

--- src/controllers/AccountsController.js
+++ src/controllers/AccountsController.js
@@ -19,23 +19,23 @@ module.exports = class AccountsController{
     }
 
     static async depositPost(req, res){
-        const amount = parseFloat(req.body.amount)
-        const type = req.body.type
-        const id = req.session.userid   
-        
-        if(isNaN(amount)){
-            req.flash('message', 'O valor informado não é um número, tente novamente!')
-            res.render('transactions/deposit')
-            return
-        }
-        
-        if(amount === 0){
-            req.flash('message', 'O valor informado é inválido, tente novamente!')
-            res.redirect('/accounts/deposit')
-            return
-        }
-
         try {
+            const amount = parseFloat(req.body.amount)
+            const type = req.body.type
+            const id = req.session.userid   
+            
+            if(isNaN(amount)){
+                req.flash('message', 'O valor informado não é um número, tente novamente!')
+                res.render('transactions/deposit')
+                return
+            }
+            
+            if(amount === 0 || amount < 0){
+                req.flash('message', 'O valor informado é inválido, tente novamente!')
+                res.redirect('/accounts/deposit')
+                return
+            }
+
             //Inserindo deposito na tabela User
             const userData = await User.findOne({where: {id}, raw: true})
             const user = {
@@ -47,10 +47,53 @@ module.exports = class AccountsController{
             //Criando registro de deposito na tabela transaction
             await Transaction.create({amount, type, receiverId: id})
 
-            req.flash('message', 'Deposito feito com sucesso!')
+            req.flash('message', `Você depositou R$ ${amount} na sua conta!`)
 
             req.session.save(() => {
                 res.redirect('/accounts/deposit')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static withdraw(req, res){
+        const balance = req.user.balance
+        res.render('transactions/withdraw', {balance})
+    }
+
+    static async withdrawPost(req, res){
+        try {
+            const id = req.session.userid
+            const amount = parseFloat(req.body.amount)
+            const type = req.body.type
+
+            console.log(typeof(amount))
+
+            if(isNaN(amount)){
+                req.flash('message', 'O valor informado não é um número, tente novamente!')
+                res.render('transactions/withdraw')
+                return
+            }
+
+            if(amount === 0 || amount < 0){
+                req.flash('message', 'O valor informado é inválido, tente novamente!')
+                res.redirect('/accounts/withdraw')
+                return
+            }
+
+            const userData = await User.findOne({where: {id}, raw: true})
+
+            const user = {balance: parseFloat(userData.balance) - amount}
+            await User.update(user, {where: {id}})
+
+            //Criando registro de saque na tabela transaction
+            await Transaction.create({amount, type, receiverId: id})
+
+            req.flash('message', `Você sacou R$ ${amount} da sua conta!`)
+
+            req.session.save(() => {
+                res.redirect('/accounts/withdraw')
             })
         } catch (error) {
             console.log(error)
